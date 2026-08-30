a9c9ca9262384e6ce80b55cf02d74b3cc8f36b89
sohail <anwarsohail412@gmail.com>
2026-08-31 01:47:14 +0530

    snakeprint,cw

---
+++ snakeprintcolumwise.java
@@ -0,0 +1,47 @@
+  import java.util.Scanner;
+public class snakeprintcolumwise {
+  
+
+    public static void main(String[] args) {
+        int arr[][]=new int[4][4];
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter element");
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                arr[i][j]=sc.nextInt();
+
+            }
+        }
+        System.out.println("Original element is : ");
+          
+        for(int i=arr.length-1;i>=0;i--){
+            for(int j=0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]+ " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("sn ake printing...");
+          
+        for(int i=0;j<arr.length;i++){
+            if(i%2==0){
+              for(int j=0;j<arr[0].length;j++){
+                 System.out.print(arr[i][j]+" ");
+              }
+
+            }
+            else{
+                for(int j=arr[0].length-1;j>=0;j--){
+                     System.out.print(arr[i][j]+ " ");
+
+                }
+
+            }
+            System.out.println();
+           
+          
+    }
+}
+}
+
+
